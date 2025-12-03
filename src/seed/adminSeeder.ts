cb30271883c01641d9ff1d800e50e4405c7badea
Nisha0202 <nishajabatunnessa@gmail.com>
2025-12-04 05:22:19 +0600

    create course

--- src/seed/adminSeeder.ts
+++ src/seed/adminSeeder.ts
@@ -1,18 +1,18 @@
-import User from '../models/User';
+// import User from '../models/User';
 
-export const createAdmin = async () => {
-  const existingAdmin = await User.findOne({ email: "admin@example.com" });
+// export const createAdmin = async () => {
+//   const existingAdmin = await User.findOne({ email: "admin@example.com" });
 
-  if (!existingAdmin) {
-    await User.create({
-      name: "Super Admin",
-      email: "admin@example.com",
-      password: "admin123",
-      role: "admin"
-    });
+//   if (!existingAdmin) {
+//     await User.create({
+//       name: "Super Admin",
+//       email: "admin@example.com",
+//       password: "admin123",
+//       role: "admin"
+//     });
 
-    console.log("✅ Admin created successfully");
-  } else {
-    console.log("⚠️ Admin already exists");
-  }
-};
+//     console.log("✅ Admin created successfully");
+//   } else {
+//     console.log("⚠️ Admin already exists");
+//   }
+// };
